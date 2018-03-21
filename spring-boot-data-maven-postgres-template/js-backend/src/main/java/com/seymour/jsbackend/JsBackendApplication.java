3d2b42b56efb1b34e59eba6fcd04e6e650ea3f0e
Work <Work@Johns-MacBook-Air.local>
2018-03-20 20:37:31 -0400

    added template

---
+++ spring-boot-data-maven-postgres-template/js-backend/src/main/java/com/seymour/jsbackend/JsBackendApplication.java
@@ -0,0 +1,12 @@
+package com.seymour.jsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsBackendApplication.class, args);
+	}
+}
